4542be8406d631f0fb26361721d35631560f9e2e
Adriana-Isabel <adriana.carrasquillo1@upr.edu>
2023-02-16 20:07:38 -0400

    Rounded rectangle's corners

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -51,7 +51,7 @@ void ofApp::drawMode1(vector<float> amplitudes) {
     ofSetColor(256); // This resets the color of the "brush" to white
     ofDrawBitmapString("Rectangle Height Visualizer", 0, 15);
     ofSetColor(0,0,ofRandom(25,255));  // rectangle's color changes to different shades of blue :)
-    ofDrawRectangle(2, ofGetHeight() - 100, 50, amplitudes[0]);
+    ofDrawRectRounded(2, ofGetHeight() - 100, 50, amplitudes[0], 10); //rounded rectangle's corners :)
     ofSetBackgroundColor(255, 182, 193); // Background color for mode 1
 }
 
